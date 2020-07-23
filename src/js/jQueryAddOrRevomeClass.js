4368dc4e7e9c653bb4982ed9df3187cf7b6bbecb
ramon <rvielma94@gmail.com>
2020-07-22 20:20:26 -0400

    Cambios para jQuery, esta faltando scroll reavel

--- src/js/jQueryAddOrRevomeClass.js
+++ src/js/jQueryAddOrRevomeClass.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-    $('.collapse.show').each(function() {
-        $(this).prev('.card-header').find('.fas').addClass('fa-minus').removeClass('fa-plus');
-    });
-    $('.collapse').on('show.bs.collapse', function() {
-        $(this).prev('.card-header').find('.fas').removeClass('fa-plus').addClass('fa-minus');
-
-    }).on('hide.bs.collapse', function() {
-        $(this).prev('.card-header').find('.fas').removeClass('fa-minus').addClass('fa-plus');
-    })
+$('.card-header').click(function() {
+    $(this).find('i').toggleClass('fas fa-plus fas fa-minus');
 });
